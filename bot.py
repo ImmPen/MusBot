1be2e4dc01e0729784709008951e9b9aafab38ca
ImmPen <ceilon865@gmail.com>
2024-02-04 17:20:58 +0300

    operating test bot

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
+from genericpath import exists
 import discord
+import json
+import os
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-    
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        print(f'Logged on as {self.user}!')
 
-client = MyClient
-client.run('MTIwMzU4MTcyNjg5MTMxMTE0NA.G9bu_9.GEGK-daD2qlnbRGXks7SBW7tnXBddmajFyQ7BI')
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+configData = {}
+
+if os.path.exists(os.getcwd()+"/config.json"):
+    with open("./config.json", "r+") as file:
+        configData = json.load(file)
+#else:
+#    configTemplate = {"Token":"", "Prefix":"!"}
+#    with open(os.getcwd()+"/config.json", "w+") as file    :
+#        json.dump(configTemplate, file)
+
+TOKEN = configData["Token"]
+PREFIX = configData["Prefix"]
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
